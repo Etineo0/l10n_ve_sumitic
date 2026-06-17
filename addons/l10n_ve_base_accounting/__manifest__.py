8c5872755b4f71631219d2eec2ba324b157f0347
Etineo0 <sumitic2205ca@gmail.com>
2026-06-17 18:54:16 -0400

    Update __manifest__.py

--- addons/l10n_ve_base_accounting/__manifest__.py
+++ addons/l10n_ve_base_accounting/__manifest__.py
@@ -5,9 +5,6 @@
     'summary': 'Configuraciones base para la contabilidad en Venezuela',
     'author': 'SUMITIC',
     'depends': ['account'],  # <--- AQUÍ ESTÁ LA CLAVE PARA QUE INSTALE CONTABILIDAD
-    'data': [
-        'data/account_chart_template.xml',
-    ],
     'installable': True,
     'application': True, # Cambiado a True para que aparezca en el menú de Apps
     'license': 'LGPL-3',
