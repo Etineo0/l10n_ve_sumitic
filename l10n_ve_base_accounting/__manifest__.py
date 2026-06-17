20c020434adfeea49902e431632092152cc45872
Etineo0 <sumitic2205ca@gmail.com>
2026-06-17 10:49:46 -0400

    Create __manifest__.py

---
+++ l10n_ve_base_accounting/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Localización Venezolana - Base Contable',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting/Localizations',
+    'summary': 'Configuraciones base para la contabilidad en Venezuela',
+    'author': 'SUMITIC',
+    'depends': ['account'],
+    'data': [
+        'data/account_chart_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
